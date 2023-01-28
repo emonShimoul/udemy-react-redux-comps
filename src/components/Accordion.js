b405cb78980c75df3326795fe2890c6654f67233
emonShimoul <emonshimoul@gmail.com>
2023-01-29 03:45:37 +0600

    apply the css

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 const Accordion = ({ items }) => {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
-        // console.log(isExpanded);
+
+        const icon = (
+            <span className='text-2xl'>
+                {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+            </span>
+        );
 
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' onClick={() => setExpandedIndex(index)}>
+                    {item.label}
+                    {icon}
+                </div>
+                {isExpanded && <div className='border-b p-5'>{item.content}</div>}
             </div>
         );
     });
 
     return (
-        <div>
+        <div className='border-x border-t rounded'>
             {renderedItems}
         </div>
     );
