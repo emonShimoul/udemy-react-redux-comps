6ac414f645d4791bc29a776ed7a897a1a31af255
emonShimoul <emonshimoul@gmail.com>
2023-01-30 01:38:07 +0600

    apply a little bit css

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
 import NavigationContext from '../context/navigation';
+import classNames from 'classnames';
 
 const Link = ({ to, children }) => {
     const { navigate } = useContext(NavigationContext);
+
+    const classes = classNames('text-blue-500');
 
     const handleClick = (event) => {
         // console.log(event);
@@ -18,7 +21,7 @@ const Link = ({ to, children }) => {
     }
 
     return (
-        <a href={to} onClick={handleClick}>{children}</a>
+        <a className={classes} href={to} onClick={handleClick}>{children}</a>
     );
 };
 
