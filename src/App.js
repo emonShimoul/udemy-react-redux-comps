4840a4596b0b3f128057d4c7e9647308155c6187
emonShimoul <emonshimoul@gmail.com>
2023-01-30 02:46:22 +0600

    make a new component for modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Modal from "./components/Modal";
 import Route from "./components/Route";
 import Sidebar from "./components/Sidebar";
 import AccordionPage from "./pages/AccordionPage";
@@ -10,14 +11,17 @@ function App() {
         <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
             <Sidebar></Sidebar>
             <div className="col-span-5">
-                <Route path="/accordion">
-                    <AccordionPage></AccordionPage>
-                </Route>
                 <Route path="/">
                     <DropdownPage></DropdownPage>
                 </Route>
+                <Route path="/accordion">
+                    <AccordionPage></AccordionPage>
+                </Route>
                 <Route path="/buttons">
                     <ButtonPage></ButtonPage>
+                </Route>
+                <Route path="/modal">
+                    <Modal></Modal>
                 </Route>
             </div>
         </div>
