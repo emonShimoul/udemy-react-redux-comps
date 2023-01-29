1edbb618b13754ebb8477b6f7bb0d4cd490b5f53
emonShimoul <emonshimoul@gmail.com>
2023-01-30 01:20:31 +0600

    create Link component using react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Link from "./components/Link";
 
 
 function App() {
     return (
-        <div></div>
+        <div>
+            <Link to="/accordion">Go to accordion</Link>
+            <Link to="/dropdown">Go to dropdown</Link>
+        </div>
     );
 }
 
