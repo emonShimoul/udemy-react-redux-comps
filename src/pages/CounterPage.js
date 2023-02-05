caeeeb68740100f11815f4e50e80b72de0f748a2
emonShimoul <emonshimoul@gmail.com>
2023-02-05 12:38:06 +0600

    implement counter with decrement button

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,16 +1,55 @@
+import { useState } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/useCounter';
+// import useCounter from '../hooks/useCounter';
+import Panel from '../components/Panel';
 
 const CounterPage = ({ initialCount }) => {
-    const { count, increment } = useCounter(initialCount);
+    // const { count, increment } = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+    const [valueToAdd, setValueToAdd] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    const handleChange = (e) => {
+        const value = parseInt(e.target.value) || 0;
+        // console.log(value);
+        setValueToAdd(value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCount(count + valueToAdd);
+        setValueToAdd(0);
+    }
 
     return (
-        <div>
-            <h1>Count is {count}</h1>
-            <Button onClick={increment}>
-                Increment
-            </Button>
-        </div>
+        <Panel className="m-3">
+            <h1 className='text-lg'>Count is {count}</h1>
+            <div className='flex flex-row'>
+                <Button onClick={increment}>
+                    Increment
+                </Button>
+                <Button onClick={decrement}>
+                    Decrement
+                </Button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!!</label>
+                <input
+                    value={valueToAdd || ""}
+                    onChange={handleChange}
+                    type="number"
+                    className='p-1 m-2 bg-gray-50 border border-gray-300' />
+                <Button>Add it!</Button>
+            </form>
+        </Panel>
     );
 };
 
